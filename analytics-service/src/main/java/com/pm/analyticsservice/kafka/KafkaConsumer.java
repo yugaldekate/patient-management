67c3ec4a08a308c235abcb7dce68b73e052d3416
Yugal D Dekate <yugaldekate72@gmail.com>
2026-03-20 15:11:45 +0530

    feat: kafka with analytics service

---
+++ analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
@@ -0,0 +1,33 @@
+package com.pm.analyticsservice.kafka;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.stereotype.Service;
+
+import patient.events.PatientEvent;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Service
+public class KafkaConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics="patient", groupId = "analytics-service")
+    public void consumeEvent(byte[] event) {
+        try {
+            PatientEvent patientEvent = PatientEvent.parseFrom(event);
+            // ... perform any business related to analytics here
+
+            log.info("Received Patient Event: [PatientId={},PatientName={},PatientEmail={}]",
+                    patientEvent.getPatientId(),
+                    patientEvent.getName(),
+                    patientEvent.getEmail());
+
+        } catch (InvalidProtocolBufferException e) {
+            log.error("Error deserializing event {}", e.getMessage());
+        }
+    }
+}
